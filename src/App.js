7000679d461058e34e0d219fa425ec99b526dfe6
josejassojr <jose.d.jassojr@gmail.com>
2021-09-25 15:42:58 -0700

    Initial styling is done.Needs functionality.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Focus Timer</header>
+      <div id="main-container">
+        <div id="session-container" className="session-break">
+          <div id="session-label" className="label">
+            Session Length
+          </div>
+          <div className="inc-dec-container">
+            {" "}
+            <button id="session-decrement" class="inc-dec-button">
+              <i class="fas fa-minus"></i>
+            </button>
+            <div id="session-length" className="length">
+              25
+            </div>
+            <button id="session-increment" class="inc-dec-button">
+              <i class="fas fa-plus"></i>
+            </button>
+          </div>
+        </div>
+        <div id="timer-container">
+          <div id="timer-label">Session</div>
+          <div id="time-left">25:00</div>
+        </div>
+        <div id="break-container" className="session-break">
+          <div id="break-label" className="label">
+            Break Length
+          </div>
+          <div className="inc-dec-container">
+            <button id="break-decrement" class="inc-dec-button">
+              <i class="fas fa-minus"></i>
+            </button>
+            <div id="break-length" className="length">
+              5
+            </div>
+            <button id="break-increment" class="inc-dec-button">
+              <i class="fas fa-plus"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="timer-controls">
+        <button id="start_stop" className="timer-btn">
+          <i class="fas fa-play"></i>
+        </button>
+        <button id="pause" className="timer-btn">
+          <i class="fas fa-pause"></i>
+        </button>
+        <button id="reset" className="timer-btn">
+          <i class="fas fa-redo"></i>
+        </button>
+      </div>
     </div>
   );
 }
